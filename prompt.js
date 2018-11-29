6b9a588b705c2cbf170a304c9000ea66dac1fa08
Nathan Baja <natalvarejao@hotmail.com>
2018-11-28 22:50:29 -0200

    Create prompt.js

---
+++ prompt.js
@@ -0,0 +1,6 @@
+var question1 = 'what is your ';
+var question2 = prompt('question age/name');
+var question = question1 + question2 + '?';
+var answer = prompt(question);
+alert('welcome to my website '+answer ); 
+
