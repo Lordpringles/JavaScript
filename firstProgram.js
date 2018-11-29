a7d3f052f8f4dc590b6637259d7241b80797cabd
Nathan Baja <natalvarejao@hotmail.com>
2018-11-28 22:47:08 -0200

    Update firstProgram.js

--- firstProgram.js
+++ firstProgram.js
@@ -7,19 +7,6 @@ console.error('YOU ARE STUPID');
 console.warn('not that stupid tho');
 console.log('%c Hello World ','color: yellow;background:purple');
 
-/*
-
-
-
-var tasks = [' 1-code', ' 2-shower', ' 3-do the dishes', ' 4-watch livestream', ' 5-go to bed',];
-var progress = ['done', ' in progress', ' to do'];
-tasks[tasks.length] = ' 6-wake up';
-                                                                                                                    to do list
-alert('choose a task from the list below');
-var question1 = prompt(tasks);
-var question2 = prompt('whats the status of the chosen task? '+progress);
-alert('you chose task '+tasks[question1-1]+' status: '+progress[question2-1]);
-*/
 
 var animals = ['dog', 'cat', 'snake', 'dinossaur', 'fly'];
 random = (Math.ceil(Math.random() *Number(animals.length))); 
