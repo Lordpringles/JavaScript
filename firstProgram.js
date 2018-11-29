920966af69c6f38b14cf72a617cec1d3a712f6b0
Nathan Baja <natalvarejao@hotmail.com>
2018-11-28 22:48:05 -0200

    Update firstProgram.js

--- firstProgram.js
+++ firstProgram.js
@@ -6,15 +6,3 @@ console.log('does this actually work?');
 console.error('YOU ARE STUPID');
 console.warn('not that stupid tho');
 console.log('%c Hello World ','color: yellow;background:purple');
-
-
-var animals = ['dog', 'cat', 'snake', 'dinossaur', 'fly'];
-random = (Math.ceil(Math.random() *Number(animals.length))); 
-var answer = prompt('Can you guess which number the animal '+animals[random-1]+' belongs to? (1-5)');
-if (answer == random && (answer  >= 1 && answer <= 5)){
-    alert('Congratulations, you guessed right');
-} else if (answer != random && (answer  >= 1 && answer <= 5)){
-    alert('Too bad, you guessed wrong');
-} else {
-    alert('Invalid option');
-}
